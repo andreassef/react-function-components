8b427f3f404cb02b992c5513bb6357dec94ffd31
andreassef <andreassef02@gmail.com>
2021-04-11 23:29:02 -0400

    [ADD] - Adicao de novos atributos e novas funcoes

--- src/components/form/Login.jsx
+++ src/components/form/Login.jsx
@@ -1,29 +1,64 @@
 import { Button, TextField } from "@material-ui/core";
+import React, { useState, useContext } from "react";
+import ValidatorsRegister from "../../contexts/ValidatorsRegister";
 
-function Login() {
+function Login({sendForm}) {
+    const [email, setEmail] = useState("");
+    const [senha, setSenha] = useState("");
+    const [erros, setErros] = useState({senha:{valido:true, texto:""}});
+
+    const validators = useContext(ValidatorsRegister);
+
+    function inputValidate(event) {
+        const {name, value} = event.target;
+        const isValid = validators[name](value);
+        const newState = {...erros};
+        newState[name] = isValid;
+        setErros(newState);
+    }
 
     return(
-        <form>
+        <form onSubmit={(event) => {
+            event.preventDefault();
+            if(erros.senha.valido){
+                sendForm({email, senha});
+            }
+        }}>
             <TextField 
+                value={email}
+                onChange={(event) => {
+                    setEmail(event.target.value)
+                }}
+                name="email"
                 id="email" 
                 label="email" 
                 type="email" 
                 variant="outlined" 
                 margin="normal" 
-                fullWidth />
+                fullWidth
+                required />
             <TextField 
+                value={senha}
+                onChange={(event) => {
+                    setSenha(event.target.value)
+                }}
+                onBlur={inputValidate}
+                error = {!erros.senha.valido}
+                helperText={erros.senha.texto}
+                name="senha"
                 id="senha" 
                 label="senha" 
                 type="password" 
                 variant="outlined" 
                 margin="normal" 
-                fullWidth />
+                fullWidth
+                required />
             <Button 
                 variant="contained" 
                 color="primary" 
                 type="submit"
             >
-                Cadastrar
+                Entrar
             </Button>
         </form>
     );
