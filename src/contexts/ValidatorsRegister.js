d414f51ec32bb33f5196e9b844169e3054ff25c5
andreassef <andreassef02@gmail.com>
2021-04-15 00:20:04 -0400

    [ADD] - Adicao de propriedades padrao no context

--- src/contexts/ValidatorsRegister.js
+++ src/contexts/ValidatorsRegister.js
@@ -1,5 +1,14 @@
 import React from 'react';
 
-const ValidatorsRegister = React.createContext();
+const ValidatorsRegister = React.createContext({
+    cpf: noValidations,
+    senha: noValidations,
+    nome: noValidations
+});
+
+function noValidations(datas) {
+    console.log(datas);
+    return {valido:true, texto: ""}
+}
 
 export default ValidatorsRegister;
